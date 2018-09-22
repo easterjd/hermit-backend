9449bce0b7687f0aea25174d0eb44bbc430fffb3
John Dillon Easter <johndilloneaster@gmail.com>
2018-09-21 17:01:04 -0700

    Modified knex file

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ const { DATABASE_URL, NODE_ENV } = process.env
 const path = require('path')
 const config = {
     client: 'pg',
-    connection: DATABASE_URL,
+    connection: 'postgres://igctqkogkeepzo:175aff6984db830db306f8fe9ccb5504f10bc374c5526723f73cc0a15000d4b2@ec2-54-227-244-12.compute-1.amazonaws.com:5432/d2jsq9gniue4cl',
     migrations: {
         directory: path.join('src', 'db', 'migrations')
     },
