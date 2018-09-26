97c47620b20b504350f652783fe44a14ebe6bc59
John Dillon Easter <johndilloneaster@gmail.com>
2018-09-26 15:24:10 -0700

    modified buzz output

--- src/routes/buzz.js
+++ src/routes/buzz.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const ctrl = require('../controllers/buzz')
 const auth = require('../lib/auth')
 
-router.post('/', auth.isLoggedIn, ctrl.getBuzz)
+router.post('/', ctrl.getBuzz)
 
 module.exports = router
